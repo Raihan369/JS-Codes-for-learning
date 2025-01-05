270757fdf509f2827f877fb0475858a3e6d9b46b
Raihan369 <88654706+Raihan369@users.noreply.github.com>
2025-01-05 12:58:32 +0000

    ok

---
+++ Basics/01_Variables.js
@@ -0,0 +1,30 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/* Variables and Data Types
+const = can't be re-assigned a value and can't be declared again  (block scope)
+let = can be re-assigned a value but can't be declared again (block scope)
+var = it can be re-assigned a value and it can also be declared again  (Global scope)
+
+=> Never use var in your code instead of let because of block and functional scope issues.
+# console.table([group of variables you want to show in tabular form]) */
